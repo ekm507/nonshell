789684992fa1c9e020a4113a3325c695266c9a12
Erfan Kheyrollahi <ekm507@gmail.com>
2022-11-21 10:31:02 +0330

    get unlimited input until sigint

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+#define MAX_LIMIT 20
+
+void handle_sigint(int sig)
+{
+    printf("Terminated.\n");
+    exit(0);
+}
 
 int main(void)
 {
+    signal(SIGINT, handle_sigint);  
+    while(1)
+    {
+        char input[MAX_LIMIT];
+        fgets(input, MAX_LIMIT, stdin);
+    }
 
-    return 0
+    return 0;
 }
