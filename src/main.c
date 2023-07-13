3a44c00c4ae595f7b367946fbe53cd6936a0e5a0
Erfan Kheyrollahi <ekm507@gmail.com>
2023-07-13 18:56:39 +0330

    update latest changes

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <string.h>
 
-#define MAX_LIMIT 20
+#define MAX_LIMIT 200
 
 void handle_sigint(int sig)
 {
@@ -12,6 +16,28 @@ void handle_sigint(int sig)
 
 int main(void)
 {
+    const char *homedir;
+
+    if ((homedir = getenv("HOME")) == NULL)
+    {
+        homedir = getpwuid(getuid())->pw_dir;
+    }
+
+    char config_filename[MAX_LIMIT];
+    strcpy(config_filename, homedir);
+    strcat(config_filename, "/.nonshell_config");
+    // printf("%s\n", config_filename);
+    FILE* config_file = fopen(config_filename, "r");
+    char filetext[MAX_LIMIT] = "";
+    char ch;
+    // while(filetext != NULL)
+    // {
+        fgets(filetext, MAX_LIMIT, config_file);
+        printf("%s", filetext);
+        // ch = fgetc(config_file);
+        // printf("%c", ch);
+    // }
+    fclose(config_file);
     signal(SIGINT, handle_sigint);  
     char input[MAX_LIMIT];
     char *check_input;
